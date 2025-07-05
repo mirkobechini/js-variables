f2db3a8034bdccc50734db7dd509a44f47fe153b
mirkobechini <mirkobechini@gmail.com>
2025-07-05 09:27:43 +0200

    add bonus-lesson exercises

---
+++ 04-bonus-lesson/javascript-basics/main.js
@@ -0,0 +1,30 @@
+const numX = 7; // NON MODIFICARE QUESTA RIGA
+const numY = 14; // NON MODIFICARE QUESTA RIGA
+const numZ = 21; // NON MODIFICARE QUESTA RIGA
+
+// Sostituisci NaN di seguito utilizzando
+// gli operatori numerici di Javascript
+
+// 1. Imposta questa variabile come somma di numX e numY
+const numXPlusNumY = NaN;
+console.log(numXPlusNumY); // Output: 21
+
+// 2. Imposta questa variabile come prodotto di numZ e numX
+const numZTimesNumX = NaN;
+console.log(numZTimesNumX); // Output: 147
+
+// 3. Imposta questa variabile come divisione di numY per numX
+const numYDividedByNumX = NaN;
+console.log(numYDividedByNumX); // Output: 2
+
+// 4. Imposta questa variabile come differenza tra numZ e numY
+const numZMinusNumY = NaN;
+console.log(numZMinusNumY); // Output: 7
+
+// 5. Imposta questa variabile come somma di numX, numY e numZ
+const combinedSum = NaN;
+console.log(combinedSum); // Output: 42
+
+// 6. Imposta questa variabile come somma di numX, numY, numZ diviso numX
+const numAverage = NaN;
+console.log(numAverage); // Output: 6
